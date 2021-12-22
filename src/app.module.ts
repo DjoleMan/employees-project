56bc30181426d2be5b82f65476ed3d50f062f430
Djordje <manojlovicdjordje91@gmail.com>
2021-12-22 10:45:54 +0100

    Initial Commit.

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { EmployeesController } from './employees/employees.controller';
+import { EmployeesModule } from './employees/employees.module';
+
+@Module({
+  imports: [EmployeesModule],
+  controllers: [AppController, EmployeesController],
+  providers: [AppService],
+})
+export class AppModule {}
