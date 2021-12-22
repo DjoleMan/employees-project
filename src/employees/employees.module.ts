f8ec9956a973d7697683ef7b9a64e78e41a2b4f9
Djordje <manojlovicdjordje91@gmail.com>
2021-12-22 14:51:34 +0100

    Add MongooseModule and MiddlewareConsumer

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -1,7 +1,28 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  RequestMethod,
+  MiddlewareConsumer,
+} from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { validateId } from './middlewares/validateId';
+import { EmployeesController } from './employees.controller';
 import { EmployeesService } from './employees.service';
+import { EmployeeSchema } from './employees.model';
 
 @Module({
-  providers: [EmployeesService]
+  imports: [
+    MongooseModule.forFeature([{ name: 'Employee', schema: EmployeeSchema }]),
+    MongooseModule.forRoot('mongodb://localhost/nestjs-employees'),
+  ],
+  controllers: [EmployeesController],
+  providers: [EmployeesService],
 })
-export class EmployeesModule {}
+export class EmployeesModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(validateId).forRoutes({
+      path: '/api/employees/*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
