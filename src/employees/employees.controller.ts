80bc9c67096d28ddfa8d9abebf6308cc3a9dff97
Djordje <manojlovicdjordje91@gmail.com>
2021-12-22 14:57:46 +0100

    Create routes.

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Put,
+  Delete,
+  Body,
+} from '@nestjs/common';
+import { EmployeeDto } from './dtos/EmployeeDto';
+import { EmployeesService } from './employees.service';
 
-@Controller('employees')
-export class EmployeesController {}
+@Controller('api/employees')
+export class EmployeesController {
+  constructor(private readonly employeesService: EmployeesService) {}
+
+  @Get()
+  async listEmployees() {
+    const employees = await this.employeesService.getEmployees();
+    return employees;
+  }
+
+  @Get(':id')
+  async getEmployee(@Param('id') id: string) {
+    const employee = await this.employeesService.getById(id);
+    if (!employee) return 'Employee not found.';
+    return employee;
+  }
+
+  @Post()
+  async insertEmployee(@Body() body: EmployeeDto) {
+    const employee = await this.employeesService.createEmployee(body);
+    return employee;
+  }
+
+  @Put(':id')
+  async updateEmployee(@Param('id') id: string, @Body() body: EmployeeDto) {
+    const employee = await this.employeesService.updateEmployee(id, body);
+    if (!employee) return 'Invalid id.';
+
+    return employee;
+  }
+
+  @Delete(':id')
+  async deleteEmployee(@Param('id') id: string) {
+    const employee = await this.employeesService.deleteEmployee(id);
+    if (!employee) return 'Invalid id';
+
+    return employee;
+  }
+}
