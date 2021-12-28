7e97b98b5ab670e3e6bf6c9a362006bf9dec6c91
Djordje <manojlovicdjordje91@gmail.com>
2021-12-28 17:00:30 +0100

    Improves EmployeeDto validation.

--- src/employees/dtos/EmployeeDto.ts
+++ src/employees/dtos/EmployeeDto.ts
@@ -1,8 +1,8 @@
-import { Optional } from '@nestjs/common';
+import { IsPostalCodeCustom } from '../helpers/validatePostalCode';
 import { Type } from 'class-transformer';
 import {
+  IsDefined,
   IsDateString,
-  IsPostalCode,
   IsEmail,
   IsPhoneNumber,
   Length,
@@ -11,34 +11,46 @@ import {
 } from 'class-validator';
 
 class HomeAddress {
+  @IsDefined({ message: 'city field is required' })
   @Length(2, 85)
   city: string;
-  @IsPostalCode()
+
+  @IsPostalCodeCustom('zip_code', { message: 'zip_code is not valid' })
+  @IsDefined({ message: 'zip_code field is required' })
   zip_code: string;
+
+  @IsDefined({ message: 'address_1 field is required' })
   @Length(4, 50)
   address_1: string;
-  @Optional()
+
+  @IsOptional()
   @Length(4, 50)
   address_2: string;
 }
 
 export class EmployeeDto {
+  @IsDefined({ message: 'name field is required' })
   @Length(2, 50)
   readonly name: string;
 
+  @IsDefined({ message: 'email_address field is required' })
   @IsEmail()
   readonly email_address: string;
 
+  @IsDefined({ message: 'phone_number field is required' })
   @IsPhoneNumber()
   readonly phone_number: string;
 
+  @IsDefined({ message: 'home_address field is required' })
   @ValidateNested()
+  @Type(() => HomeAddress)
   readonly home_address: HomeAddress;
 
   @IsOptional()
   @IsDateString()
   date_of_employment: string;
 
+  @IsDefined({ message: 'date_of_birth field is required' })
   @IsDateString()
   date_of_birth: string;
 }
