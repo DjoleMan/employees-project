e68fe2545e25e963be375526766672186eb7bd1e
Djordje <manojlovicdjordje91@gmail.com>
2021-12-28 17:03:55 +0100

    Exclude validateId middleware.

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -1,11 +1,6 @@
-import {
-  Module,
-  NestModule,
-  RequestMethod,
-  MiddlewareConsumer,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { validateId } from './middlewares/validateId';
+
 import { EmployeesController } from './employees.controller';
 import { EmployeesService } from './employees.service';
 import { EmployeeSchema } from './employees.model';
@@ -18,11 +13,4 @@ import { EmployeeSchema } from './employees.model';
   controllers: [EmployeesController],
   providers: [EmployeesService],
 })
-export class EmployeesModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(validateId).forRoutes({
-      path: '/api/employees/*',
-      method: RequestMethod.ALL,
-    });
-  }
-}
+export class EmployeesModule {}
