a9581d7cdf4a00998e03710f1f7ea6404112840c
Djordje <manojlovicdjordje91@gmail.com>
2021-12-31 13:54:05 +0100

    Define argument type for methods.

--- src/employees/helpers/dates.ts
+++ src/employees/helpers/dates.ts
@@ -1,6 +1,7 @@
 import * as moment from 'moment';
+import { EmployeeDto } from '../dtos/employee.dto';
 
-export function dateEmployment(employee) {
+export function dateEmployment(employee: EmployeeDto) {
   if (employee.date_of_employment !== undefined) {
     let timestamp = Date.parse(employee.date_of_employment);
     return moment(timestamp).format('YYYY-MM-DD');
@@ -8,7 +9,7 @@ export function dateEmployment(employee) {
   return moment().format('YYYY-MM-DD');
 }
 
-export function dateBirth(employee) {
+export function dateBirth(employee: EmployeeDto) {
   let timestamp = Date.parse(employee.date_of_birth);
   return moment(timestamp).format('YYYY-MM-DD');
 }
