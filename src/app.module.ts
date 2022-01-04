3d6911216f15c92971d44a7ecf608b24a967ec9d
Djordje <manojlovicdjordje91@gmail.com>
2022-01-04 12:17:31 +0100

    Use .env file to connect to database.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [EmployeesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<String>('DATABASE'),
+      }),
+      inject: [ConfigService],
+    }),
+    EmployeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
